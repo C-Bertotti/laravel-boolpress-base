0b48f6cb909afaf0e21e414f1f215a4db8420007
Caterina Bertotti <bertotti4997@gmail.com>
2021-05-27 17:42:44 +0200

    Corretta validazione metodo Create

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -18,6 +18,8 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all();
+
+        return view('admin.posts.index', compact('posts'));
     }
 
     /**
@@ -38,31 +40,41 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
+
         //validazione
-        $data = $request->all();
-
-        if (!isset($data['published'])) {
-            $data['published'] = false;
-        } else {
-            $data['published'] = true;
-        }
-
+    
+        //1. Validazione dei dati inseriti
         $request->validate([
             'title' => 'required|max:255|string',
             'date' => 'required|date',
             'content' => 'required|string',
             'image' => 'nullable|url',
         ]);
+        
+        //2.salvo i dati in una variabile d'appoggio
+        $data = $request->all();
 
-        //insert
+        //3. Faccio un controllo sulla checkbox. La chiave  "published" è settata? allora inserisco come valore della chiave 1, altrimenti 0
+        $data['published'] = isset($data['published']) ? 1 : 0;
+
+        //4. Se passa la validazione vado a fare l'operazione di
+
+        //INSERT
+
+        //1. Istanzio un nuovo elemento sulla base del modello Post (mi ricordo di inserire il Model nella parte alta del documento con Use 'App\Post')
         $newPost = new Post();
+
+        //2. Attribuisco i le chiavi-valore all'istanza
         $newPost->title = $data['title'];
         $newPost->date = $data['date'];
         $newPost->content = $data['content'];
         $newPost->image = $data['image'];
         $newPost->slug = Str::slug($data['title'], '-');
         $newPost->published = $data['published'];
-        $newPost->save();
+
+        $newPost->save(); 
+
+        //NB: Se non avessimo dovuto manipolare lo slug avremmo potuto semplicemente scrivere Post::create($data), altriment avremo potuto usarlo ma solamente se avessimo assegnato il valore slug al di fuori sotto la variabile d'appoggio $data['slug'] = Str::slug($data['title'], '-');
 
         //reindirizzamento
         return redirect()->route('admin.posts.index');
@@ -76,9 +88,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //visualizzo i commenti 
-        dd($post->comments);
-
+        //
     }
 
     /**
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -5,33 +5,45 @@
 @endsection
 
 @section('content')
-<form action="{{route('admin.post.store')}}" method="POST">
-    @method('POST')
-    @csrf
-    <div class="form-group">
-      <label for="title">Titolo</label>
-      <input type="text" class="form-control" id="title" name="title" placeholder="Title">
-    </div>
-    <div class="form-group">
-        <label for="date">Data</label>
-        <input type="date" class="form-control" id="date" name="date" placeholder="Date">
-    </div>
-    <div class="form-group">
-        <label for="content">Contenuto</label>
-        <textarea class="form-control" name="content" id="content" rows="10" placeholder="Content"></textarea>
-    </div>
-    <div class="form-group">
-        <label for="image">Immagine</label>
-        <input type="text" class="form-control" id="image" name="image">
-    </div>
-    
-    <!-- checkbox -->
-    <div class="form-check ">
-        <input class="form-check-input" type="checkbox" id="published" name="published" placeholder="published">
-        <label class="form-check-label" for="published">Pubblicato</label>
-    </div>
-    <!-- /checkbox -->
+    <!-- stampo, se ci sono, lista di errori -->
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <!-- /stampo, se ci sono, lista di errori -->
 
-    <button type="submit" class="btn btn-primary mb-5 mt-5">Crea</button>
-  </form>
+    <form action="{{route('admin.post.store')}}" method="POST">
+        @method('POST')
+        @csrf
+        <div class="form-group">
+        <label for="title">Titolo</label>
+        <input type="text" class="form-control" id="title" name="title" placeholder="Title" value="{{ old('title') }}">
+        </div>
+        <div class="form-group">
+            <label for="date">Data</label>
+            <input type="date" class="form-control" id="date" name="date" placeholder="Date" value="{{ old('date') }}">
+        </div>
+        <div class="form-group">
+            <label for="content">Contenuto</label>
+            <textarea class="form-control" name="content" id="content" rows="10" placeholder="Content">{{ old('content') }}</textarea>
+        </div>
+        <div class="form-group">
+            <label for="image">Immagine</label>
+            <input type="text" class="form-control" id="image" name="image" value="{{ old('image') }}" placeholder="Inserisci l'URL dell'immagine">
+        </div>
+        
+        <!-- checkbox -->
+        <div class="form-check ">
+            <input class="form-check-input" type="checkbox" id="published" name="published" placeholder="published">
+            <label class="form-check-label" for="published">Pubblicato</label>
+        </div>
+        <!-- /checkbox -->
+
+        <button type="submit" class="btn btn-primary mb-5 mt-5">Crea</button>
+    </form>
 @endsection
