0017f8cda9551fd4cd87e50ca46aa943f79257a1
Caterina Bertotti <bertotti4997@gmail.com>
2021-05-27 16:38:38 +0200

    Inserita validazione - visualizzazione degli errori

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Post;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
+
 
 class PostController extends Controller
 {
@@ -36,7 +38,34 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validazione
+        $data = $request->all();
+
+        if (!isset($data['published'])) {
+            $data['published'] = false;
+        } else {
+            $data['published'] = true;
+        }
+
+        $request->validate([
+            'title' => 'required|max:255|string',
+            'date' => 'required|date',
+            'content' => 'required|string',
+            'image' => 'nullable|url',
+        ]);
+
+        //insert
+        $newPost = new Post();
+        $newPost->title = $data['title'];
+        $newPost->date = $data['date'];
+        $newPost->content = $data['content'];
+        $newPost->image = $data['image'];
+        $newPost->slug = Str::slug($data['title'], '-');
+        $newPost->published = $data['published'];
+        $newPost->save();
+
+        //reindirizzamento
+        return redirect()->route('admin.posts.index');
     }
 
     /**
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -9,25 +9,29 @@
     @method('POST')
     @csrf
     <div class="form-group">
-      <label for="title">Title</label>
+      <label for="title">Titolo</label>
       <input type="text" class="form-control" id="title" name="title" placeholder="Title">
     </div>
     <div class="form-group">
-        <label for="date">date</label>
+        <label for="date">Data</label>
         <input type="date" class="form-control" id="date" name="date" placeholder="Date">
     </div>
     <div class="form-group">
-        <label for="content">Content</label>
+        <label for="content">Contenuto</label>
         <textarea class="form-control" name="content" id="content" rows="10" placeholder="Content"></textarea>
     </div>
     <div class="form-group">
-        <label for="image">image</label>
+        <label for="image">Immagine</label>
         <input type="text" class="form-control" id="image" name="image">
     </div>
-    <div class="form-group">
-        <label for="published">published</label>
-        <input type="checkbox" class="form-control" id="published" name="published" placeholder="published">
+    
+    <!-- checkbox -->
+    <div class="form-check ">
+        <input class="form-check-input" type="checkbox" id="published" name="published" placeholder="published">
+        <label class="form-check-label" for="published">Pubblicato</label>
     </div>
-    <button type="submit" class="btn btn-primary">Crea</button>
+    <!-- /checkbox -->
+
+    <button type="submit" class="btn btn-primary mb-5 mt-5">Crea</button>
   </form>
 @endsection
