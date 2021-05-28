99b260a4d85733cc9541cb753f744e9e6fddd2e2
Caterina Bertotti <bertotti4997@gmail.com>
2021-05-28 08:47:26 +0200

    ADMIN: aggiunta pagina e metodo show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,1 +1,18 @@
-@dump($post)
+@extends('layouts.base')
+
+@section('pageTitle')
+    {{ $post->title }}
+@endsection
+
+@section('content')
+    <div class=" mb-5">
+        <img class="cover__image--show" src="{{ $post->image }}" alt="{{ $post->title }}">
+    </div>
+    <div>
+        <p>{{ $post->content }}</p>
+    </div>
+    <div class="text-right">
+        <a href="{{ route('admin.post.index') }}"><button type="button" class="btn btn-primary">Back</button></a>
+    </div>
+@endsection
+
